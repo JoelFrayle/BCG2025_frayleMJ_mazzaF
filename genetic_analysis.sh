03e8e107f630b39967e2a673a86ab97cc3e0aee5
Joel Frayle <151047529+JoelFrayle@users.noreply.github.com>
2025-04-09 14:20:39 +0200

    Update genetic_analysis.sh

--- genetic_analysis.sh
+++ genetic_analysis.sh
@@ -1,235 +1,130 @@
-## Exam
-mkdir genomics2025_exam
-cd genomics2025_exam/
-mkdir AD
-cd AD
-mkdir case731
-mkdir case593
-mkdir case671
-cd genomics2025_exam/
-mkdir AR
-cd AR
-mkdir case732
-mkdir case739
-cd AD
-
-#Case731
-
-cd case731
-ln -s /home/BCG2025_genomics_exam/case731_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case731_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case731_mother.fq.gz
-fastqc *.fq.gz
-
-nohup bowtie2 -U case731_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case731_child.bam &
-nohup bowtie2 -U case731_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case731_father.bam &
-nohup bowtie2 -U case731_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case731_mother.bam &
-
-samtools index case731_child.bam
-samtools index case731_father.bam
-samtools index case731_mother.bam
-
-nohup freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case731_child.bam case731_father.bam case731_mother.bam > Trio731.vcf &
-
-bcftools view -i 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio731.vcf > candidates731.vcf
-
-Then e! and:
-https://grch37.ensembl.org/Homo_sapiens/Tools/VEP/Results?field1=IMPACT;from=1;operator1=is;size=37;tl=8Z57E8H81aJbnwp7-10915062;to=37;value1=HIGH#Results
-
-TOWNES-BROCKS SYNDROME 1
-
-#Case732
-cd case 732
-ln -s /home/BCG2025_genomics_exam/case732_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case732_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case732_mother.fq.gz
-fastqc *.fq.gz
-
-nohup bowtie2 -U case732_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case732_child.bam &
-nohup bowtie2 -U case732_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case732_father.bam &
-nohup bowtie2 -U case732_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case732_mother.bam &
-
-samtools index case732_child.bam
-samtools index case732_father.bam
-samtools index case732_mother.bam
-
-nohup freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case732_child.bam case732_father.bam case732_mother.bam > Trio732.vcf &
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio732.vcf > candidates732.vcf
-No dio ningun resultado asi que probamos con 
-bcftools view -i 'GT[2]="0/0" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio732.vcf > candidates732.vcf
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio732.vcf > candidates732.vcf
-REVISAAAAR!!!!!!!!!
-https://grch37.ensembl.org/Homo_sapiens/Tools/VEP/Results?field1=PHENO;from=1;operator1=is;size=167;tl=cYkiKVSQf5HNoAfc-10915065;to=167;value1=#Results
-
-#Case593
-cd case593
-ln -s /home/BCG2025_genomics_exam/case593_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case593_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case593_mother.fq.gz
-fastqc *.fq.gz
-
-nohup bowtie2 -U case593_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case593_child.bam &
-nohup bowtie2 -U case593_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case593_father.bam &
-nohup bowtie2 -U case593_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case593_mother.bam &
-
-samtools index case593_child.bam
-samtools index case593_father.bam
-samtools index case593_mother.bam
-
-nohup freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case593_child.bam case593_father.bam case593_mother.bam > Trio593.vcf &
-
-bcftools view -i 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio593.vcf > candidates593.vcf
-
-https://grch37.ensembl.org/Homo_sapiens/Tools/VEP/Results?field1=IMPACT;operator1=is;time=1743002605227.227;tl=Y3hL2VvwrNQhbNb7-10915059;value1=HIGH#Results
-Autosomal dominant polycystic kidney disease adult type
-
-#case739
-cd case 739
-ln -s /home/BCG2025_genomics_exam/case739_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case739_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case739_mother.fq.gz
-fastqc *.fq.gz
-
-nohup bowtie2 -U case739_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case739_child.bam &
-nohup bowtie2 -U case739_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case739_father.bam &
-nohup bowtie2 -U case739_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case739_mother.bam &
-
-samtools index case739_child.bam
-samtools index case739_father.bam
-samtools index case739_mother.bam
-
-nohup freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case739_child.bam case739_father.bam case739_mother.bam > Trio739.vcf &
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio739.vcf > candidates739.vcf
-
-
-https://grch37.ensembl.org/Homo_sapiens/Tools/VEP/Results?field1=IMPACT;from=1;operator1=is;size=653;tl=qL4QIc9xEiQF4VMN-10915067;to=653;value1=HIGH#Results
-
-Fanconi anemia
-
-#Case671
-cd case671
-ln -s /home/BCG2025_genomics_exam/case671_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case671_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case671_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case671_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case671_child.bam
-bowtie2 -U case671_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case671_father.bam
-bowtie2 -U case671_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case671_mother.bam
-
-samtools index case671_child.bam
-samtools index case671_father.bam
-samtools index case671_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case671_child.bam case671_father.bam case671_mother.bam > Trio671.vcf
-
-bcftools view -i 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio671.vcf > candidates671.vcf
-
-https://grch37.ensembl.org/Homo_sapiens/Tools/VEP/Results?from=1;size=35;tl=t7iEBif6QbgDNGQS-10915061;to=35#Results
-Tuberous sclerosis syndrome
-
-
-#########################################################################
-Mazza
-#Case697 AR
-cd case697
-ln -s /home/BCG2025_genomics_exam/case697_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case697_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case697_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case697_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case697_child.bam
-bowtie2 -U case697_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case697_father.bam
-bowtie2 -U case697_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case697_mother.bam
-
-samtools index case697_child.bam
-samtools index case697_father.bam
-samtools index case697_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case697_child.bam case697_father.bam case697_mother.bam > Trio697.vcf
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio697.vcf > candidates697.vcf
-
-
-#case738 AD
-cd case738
-ln -s /home/BCG2025_genomics_exam/case738_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case738_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case738_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case738_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case738_child.bam
-bowtie2 -U case738_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case738_father.bam
-bowtie2 -U case738_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case738_mother.bam
-
-samtools index case738_child.bam
-samtools index case738_father.bam
-samtools index case738_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case738_child.bam case738_father.bam case738_mother.bam > Trio738.vcf
-
-bcftools view -i 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio738.vcf > candidates738.vcf
-
-
-#Case641 AR
-cd case641
-ln -s /home/BCG2025_genomics_exam/case641_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case641_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case641_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case641_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case641_child.bam
-bowtie2 -U case641_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case641_father.bam
-bowtie2 -U case641_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case641_mother.bam
-
-samtools index case641_child.bam
-samtools index case641_father.bam
-samtools index case641_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case641_child.bam case641_father.bam case641_mother.bam > Trio641.vcf
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio641.vcf > candidates641.vcf
-
-
-#Case628 AR
-cd case628
-ln -s /home/BCG2025_genomics_exam/case628_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case628_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case628_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case628_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case628_child.bam
-bowtie2 -U case628_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case628_father.bam
-bowtie2 -U case628_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case628_mother.bam
-
-samtools index case628_child.bam
-samtools index case628_father.bam
-samtools index case628_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case628_child.bam case628_father.bam case628_mother.bam > Trio628.vcf
-
-bcftools view -i 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20' Trio628.vcf > candidates628.vcf
-
-
-#Case689 AD
-
-cd case689
-ln -s /home/BCG2025_genomics_exam/case689_child.fq.gz
-ln -s /home/BCG2025_genomics_exam/case689_father.fq.gz
-ln -s /home/BCG2025_genomics_exam/case689_mother.fq.gz
-fastqc *.fq.gz
-
-bowtie2 -U case689_child.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SC' --rg 'SM:Child' |samtools view -Sb |samtools sort -o case738_child.bam
-bowtie2 -U case689_father.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SF' --rg 'SM:Father' |samtools view -Sb |samtools sort -o case738_father.bam
-bowtie2 -U case689_mother.fq.gz -x /home/BCG2025_genomics_exam/uni --rg-id 'SM' --rg 'SM:Mother' |samtools view -Sb |samtools sort -o case738_mother.bam
-
-samtools index case689_child.bam
-samtools index case689_father.bam
-samtools index case689_mother.bam
-
-freebayes -f /home/BCG2025_genomics_exam/universe.fasta -t /home/BCG2025_genomics_exam/exons16Padded_sorted.bed -m 20 -C 5 -Q 10 --min-coverage 10 case689_child.bam case689_father.bam case689_mother.bam > Trio689.vcf
-
-bcftools view -i 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20' Trio689.vcf > candidates689.vcf
+cat << 'EOF' > run_pipeline.py
+import os
+import subprocess
+
+# Set up base directories and file paths
+base_dir = "genomics2025_exam"  # Main folder for results
+data_dir = "/home/BCG2025_genomics_exam"  # Folder where raw input files are stored
+reference_index = os.path.join(data_dir, "uni")  # Bowtie2 reference index
+reference_fasta = os.path.join(data_dir, "universe.fasta")  # Reference genome FASTA
+target_bed = os.path.join(data_dir, "exons16Padded_sorted.bed")  # Target regions in BED format
+
+# Define case groups by inheritance mode
+# Frayle cases: 593, 671, 731, 732, 739
+# Mazza cases: 628, 641, 689, 697 738
+cases = {
+    "AD": ["case731", "case593", "case671", "case738", "case689"],
+    "AR": ["case732", "case739", "case697", "case641", "case628"]
+}
+
+# Function to run a shell command (optionally in the background)
+def run_command(cmd, cwd=None, background=False):
+    print(f"Running: {cmd}")
+    if background:
+        process = subprocess.Popen(cmd, shell=True, cwd=cwd,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f"Process started with PID: {process.pid}")
+        return process
+    else:
+        subprocess.run(cmd, shell=True, check=True, cwd=cwd)
+
+# Get a VCF filter string for bcftools based on the mode
+def get_vcf_filter(mode):
+    if mode == "AD":
+    # For Autosomical dominant the child suffers a mutation, so non of the parents have the alele, but the child does. The order in our aligment is mom, dad and child
+    # so GT[0] = mother, GT[1] = father and GT[2] = child
+        return 'GT[2]="0/1" && GT[0]="0/0" && GT[1]="0/0" && QUAL>=20'
+    elif mode == "AR":
+    # For autosomical recesive both of parents are heterozygotes and the child homozygote 
+        return 'GT[2]="1/1" && GT[0]="0/1" && GT[1]="0/1" && QUAL>=20'
+    return None
+
+# Create main folders if they don't exist
+os.makedirs(base_dir, exist_ok=True)
+fastqc_results_dir = os.path.join(base_dir, "fastqc_results")
+os.makedirs(fastqc_results_dir, exist_ok=True)
+multiqc_report_dir = os.path.join(base_dir, "multiqc_report")  # To be generated by MultiQC
+cases_dir = os.path.join(base_dir, "cases")
+os.makedirs(cases_dir, exist_ok=True)
+
+# Global analysis: prepare FastQC symlinks and run FastQC/MultiQC
+print("Starting global FastQC analysis...")
+all_fq_links = []
+for mode, case_list in cases.items():
+    for case in case_list:
+        for role in ["child", "father", "mother"]:
+            fq_file = f"{case}_{role}.fq.gz"
+            fq_path = os.path.join(data_dir, fq_file)
+            link_path = os.path.join(fastqc_results_dir, fq_file)
+            if not os.path.exists(link_path):
+                os.symlink(fq_path, link_path)
+            # Make the link path relative to base_dir so FastQC finds it correctly
+            rel_link = os.path.relpath(link_path, base_dir)
+            all_fq_links.append(rel_link)
+
+# Run FastQC and then MultiQC, using base_dir as the current working directory
+run_command(f"fastqc {' '.join(all_fq_links)} -o fastqc_results", cwd=base_dir)
+run_command(f"multiqc fastqc_results -o multiqc_report", cwd=base_dir)
+
+# Process each case, grouping them by inheritance mode
+for mode, case_list in cases.items():
+    mode_dir = os.path.join(cases_dir, mode)
+    os.makedirs(mode_dir, exist_ok=True)
+    
+    for case in case_list:
+        case_path = os.path.join(mode_dir, case)
+        os.makedirs(case_path, exist_ok=True)
+        print(f"Processing {case} in {mode}...")
+        
+        # Create local symlinks for the FASTQ files in this case folder
+        for role in ["child", "father", "mother"]:
+            fq_file = f"{case}_{role}.fq.gz"
+            fq_path = os.path.join(data_dir, fq_file)
+            link_path = os.path.join(case_path, fq_file)
+            if not os.path.exists(link_path):
+                os.symlink(fq_path, link_path)
+
+        # Run Bowtie2 alignment for each role (child, father, mother) in parallel
+        processes = []
+        for role, rgid in zip(["child", "father", "mother"], ["SC", "SF", "SM"]):
+            fq = f"{case}_{role}.fq.gz"
+            bam = f"{case}_{role}.bam"
+            log_file = f"bowtie2_{case}_{role}.log"
+            cmd = (
+                f"bowtie2 -U {fq} -x {reference_index} --rg-id '{rgid}' --rg 'SM:{role.capitalize()}' "
+                f"| samtools view -Sb | samtools sort -o {bam} > {log_file} 2>&1"
+            )
+            process = run_command(cmd, cwd=case_path, background=True)
+            processes.append(process)
+            
+        # Wait for all alignment processes to finish
+        for process in processes:
+            process.wait()
+
+        # Index the BAM files using samtools
+        for role in ["child", "father", "mother"]:
+            bam = f"{case}_{role}.bam"
+            run_command(f"samtools index {bam}", cwd=case_path)
+
+        # Run variant calling with Freebayes
+        trio_bams = " ".join([f"{case}_{role}.bam" for role in ["child", "father", "mother"]])
+        vcf_output = f"Trio{case[-3:]}.vcf"
+        cmd = (
+            f"freebayes -f {reference_fasta} -t {target_bed} -m 20 -C 5 -Q 10 --min-coverage 10 {trio_bams} > {vcf_output}"
+        )
+        run_command(cmd, cwd=case_path)
+        
+        # Filter the VCF file with bcftools using the appropriate filter for the mode
+        vcf_filter = get_vcf_filter(mode)
+        if vcf_filter:
+            run_command(
+                f"bcftools view -i '{vcf_filter}' {vcf_output} > candidates{case[-3:]}.vcf",
+                cwd=case_path
+            )
+            
+        print(f"Finished processing {case}.")
+
+print("Pipeline completed! Check the multiqc_report and case-specific folders.")
+EOF
+# Then we run the pipeline in the background
+nohup python3 run_pipeline.py &
